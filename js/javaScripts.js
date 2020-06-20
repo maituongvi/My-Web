04cd2590c7faa088c3b5bdb59c5b97cae8f9a187
myhoa <lisagomezusa99.com>
2020-06-20 09:18:25 +0700

    The last time

--- js/javaScripts.js
+++ js/javaScripts.js
@@ -1,3 +1,4 @@
+// Nut Scroll to Stop
 $(document).ready(function(e) {
 	 $(window).scroll(function(){
 		if ($(this).scrollTop() > 100) {
@@ -12,7 +13,7 @@ $(document).ready(function(e) {
 		return false;
 	});
 });
-
+// Thanh search tren trang chu
 function suggestProduct(obj) {
 	var product = ["MAX&CO - Khăn choàng Double Sided","MUJOSH - Kính mát tròn khuyết cạnh Avant Garde","MUJOSH - Kính mát thông minh chống nắng tối ưu","TED BAKER - Túi đựng dụng cụ trang điểm Eulali","TED BAKER - Ốp lưng điện thoại iphone XR Abbieey","TED BAKER - Ốp lưng điện thoại iphone 6,7,8 Plus","MUJOSH - Kính mát thông minh chống nắng tối ưu","MUJOSH - Kính mát đa giác thời thượng","PUMA","Aojo - Kính mát bé gái hình con voi dễ thương","MUJOSH - Kính mát đa giác bo tròn thời trang","DSQUARED2 - Nón bóng chày Dsquared2 1964","DSQUARED2 - Nón nam thêu cá tính Logo Patch","DSQUARED2","Nón nam thêu cá tính Canada Plate","Leather and House Check High-top Sneakers","Leather, Vintage Check Cotton and Suede Tor Boots","Men's Disney x Gucci Rhyton","Men's Rhyton Gucci Worldwide","Women's Gucci Tennis 1977","Air Jordan 1 Mid SE","Air Jordan XXXIII","Nike Air Max 97","VANS CHECKERBOARD SLIP-ON CLASSIC BLACKWHITE","VANS OLD SKOOL CLASSIC BLACKWHITE","Chuck Taylor All Star Chuck Taylor Cheerful Hi Top","Chuck Taylor All Star Twisted Classic Logo Play Hi","Monogram Print E-canvas Arthur","Ultraboost SandL Star Wars","Giay UltraBoost MTL","Flight by VietMax","Ivory Black","LEBY LEMINO - Túi Sách Nữ","PEDRO - Hiệu ứng trong suốt","Woven Zip Around Wallet","Snake Print Woven Shoulder Bag","Glitter Top Handle Envelope Bag","Angular Flap Shoulder Bag","Túi Angular Flap Metallic Top Handle" ,"Túi Small Two-Tone Sculptural" ,"Túi Metal Top Handle Long Wallet" ,"Túi Woven Shoulder ","Túi Small Chunky Chain Strap ","Túi Grommet Strap Tote" ,"Túi Curved Tote","Túi Double Top Handle Tote","Túi Textured Crossbody","Túi Leather Saddle Bag","Túi Triangle Handle Tote Bag","Túi Metallic Accent Mini Wallet","Túi Pleated Drawstring Bag","Túi Textured Zip Around Wallet"
 
@@ -30,7 +31,7 @@ function suggestProduct(obj) {
 
 }
 
-
+// zoom hình to lên trong sản phẩm
  function zoom(e) {
 		    var zoomer = e.currentTarget;
 		    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
--- js/scroll.js
+++ js/scroll.js
@@ -2,10 +2,8 @@ $(document).ready(function(e) {
 	 $(window).scroll(function(){
 		if ($(this).scrollTop() > 100) {
 			$('#scrollup').fadeIn();
-			$('div.menu').css({'position':'fixed','top':0,'left':0,'width':'100%','z-index':100});
 		} else {
 			$('#scrollup').fadeOut();
-			$('div.menu').css({'position':'relative','top':'auto','left':'auto'});
 		}
 		}); 
 
